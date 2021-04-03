f61254f3aa77ffb454484c32312476db82821625
Michellejacob001 <62324802+Michellejacob001@users.noreply.github.com>
2021-04-03 15:55:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+let addToDOButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDOButton.addEventListener('click', function(){
+var paragraph = document.createElement('p');
+paragraph.classList.add('paragraph-styling');
+paragraph.innerText = inputField.value;
+toDoContainer.appendChild(paragraph);
+inputField.value =" ";
+paragraph.addEventListener('click', function(){
+paragraph.style.textDecoration = "line-through";
+})
+paragraph.addEventListener('dblclick', function(){
+    toDoContainer.removeChild(paragraph);
+    })
+})
